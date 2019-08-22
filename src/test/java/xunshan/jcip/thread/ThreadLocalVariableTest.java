79d9827e1a77935c47af129be1c7028940ebc1b9
lihuangdong@mozat.com <lihuangdong@mozat.com>
2019-08-22 14:48:10 +0800

    update

--- src/test/java/xunshan/jcip/thread/ThreadLocalVariableTest.java
+++ src/test/java/xunshan/jcip/thread/ThreadLocalVariableTest.java
@@ -20,4 +20,30 @@ public class ThreadLocalVariableTest {
 		t.start();
 		t.join();
 	}
+
+	// common pattern
+	@Test
+	public void getAndSet() throws InterruptedException {
+		ThreadLocal<String> tlStr = new ThreadLocal<>();
+		final String str = "hi";
+
+		// a new session
+		Thread t = new Thread(() -> {
+			try {
+				// before request
+				tlStr.set(str);
+
+				// handle request
+				assertEquals(str, tlStr.get());
+			} finally {
+				// clear context
+				tlStr.remove();
+
+				assertNull(tlStr.get());
+			}
+		});
+
+		t.start();
+		t.join();
+	}
 }
